65c11001fca5dbd5b43878b6647fba1a548a371c
lauravacca81 <33028379+lauravacca81@users.noreply.github.com>
2017-12-11 15:12:29 +0100

    Add files via upload

---
+++ agenzia-viaggi/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<style type="text/css">
+		img {
+			height: 450px;
+		}
+		nav {
+			margin-top: 25px;
+			margin-bottom: 25px;
+		}
+
+	</style>
+	<title></title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md">
+			<? php include ('navbar.php'); ?>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-2">
+				One of three columns
+			</div>
+			<div class="col-md-8">
+			<? php include ('carousel.php'); ?>	
+				</div>	
+				<div class="col-md-2">
+					One of three columns
+				</div>
+			</div>
+		</div>
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+	</body>
+	</html>
